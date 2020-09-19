4aa1ffc708227d2065a9674359377fb240b2501f
Utkarsh220900 <uakhouri2000@gmail.com>
2020-09-19 19:43:06 +0530

    pharmacy near me updated

--- app.py
+++ app.py
@@ -1,21 +1,55 @@
 from flask import Flask, render_template, request
+import json
+from pharmnearme import searchparams
+
 app = Flask(__name__)
 
 # localhost:5000/
 
 
 @app.route('/', methods=['GET'])
+def hello():
+    return render_template('main.html')
+
+
+@app.route('/api/enter', methods=['GET'])
 def hello_world():
     return render_template('index.html')
 
 # localhost:5000/api/disease
 
 
+@app.route('/nearmeshow', methods=['GET'])
+def searching():
+    return render_template('nearme_del.html')
+
+
+@app.route('/nearmerun', methods=['POST'])
+def queryInput():
+    req_data = str(request.get_data(['cityname']))
+
+    return json.dumps(searchparams(req_data))
+
+    # print(req_data)
+    # return "True"
+
+
 @app.route('/api/disease', methods=['POST'])
 def getDisease():
     req_data = request.get_json()
-    print(req_data)
+    print(req_data['symptoms'])
+
     return "You have fever"
+
+
+@app.route('/api/symptoms', methods=['GET'])
+def getSymptoms():
+
+    symptoms = ['Sym A', 'Sym B', 'Sym C']
+    data = {
+        "symptoms": symptoms
+    }
+    return json.dumps(data)
 
 
 app.run()
