dbc7115f30559525c1c25c8421d7005ca1a721ba
Utkarsh220900 <uakhouri2000@gmail.com>
2020-09-20 07:20:13 +0530

    mlandbackend

--- databasecomm.py
+++ databasecomm.py
@@ -2,15 +2,19 @@ import psycopg2
 import os
 import urllib.parse as up
 
-up.uses_netloc.append("postgres")
-url = 'postgres://rciudlch:f1cUXgsORypockj4aiu2o3ZsM2QiUTl4@john.db.elephantsql.com:5432/rciudlch'
-conn = psycopg2.connect(url)
 
-cursor = conn.cursor()
-msg1 = """INSERT INTO ENTRYUSER (ID,NAME,GENDER,AGE,DISEASE)
-                VALUES(2,'UTKARSH','M',19,'HEALTHY');"""
-cursor.execute(msg1)
-conn.commit()
+def databaseadd(name, gender, age, disease):
+    print(name, gender, age, disease)
+    up.uses_netloc.append("postgres")
+    url = "postgres://rciudlch:f1cUXgsORypockj4aiu2o3ZsM2QiUTl4@john.db.elephantsql.com:5432/rciudlch"
+    conn = psycopg2.connect(url)
+    print("true")
+    cursor = conn.cursor()
 
+    msg1 = "INSERT INTO ENTRYUSER (NAME,GENDER,AGE,DISEASE)  VALUES('" + \
+        name+"','"+gender+"',"+age+",'"+disease+"');"
+    print(msg1)
+    cursor.execute(msg1)
+    conn.commit()
 
-conn.close()
+    conn.close()
